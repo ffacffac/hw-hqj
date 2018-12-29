5a1ca77c010137544e10adf252cb3164f88fa06a
hqj <185138375@qq.com>
2018-12-29 11:02:03 +0800

    提交基于xmpp协议的Android客户端代码

---
+++ androidpn-client/src/org/androidpn/config/Config.java
@@ -0,0 +1,8 @@
+package org.androidpn.config;
+
+public class Config {
+
+	public static final String serverIp = "192.168.0.166";
+	public static final String port = "5222";
+	public static final String apiKey = "1234567890";
+}
