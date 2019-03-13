3e1705bbc913eced5095da165fb393520091699b
hqj <185138375@qq.com>
2019-03-13 14:08:56 +0800

    很不错的加载loading动画，像菊花一样转动

---
+++ JhProressbar/src/com/example/jhproressbar/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.jhproressbar;
+
+import com.example.jhproressbar.view.BaseProgressDialog;
+import com.example.jhproressbar.view.ProgressDialog;
+import com.example.jhproressbar.view.ProgressLoadDialog;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	Button indeterminate = (Button) findViewById(R.id.indeterminate);
+	indeterminate.setOnClickListener(this);
+
+	Button labelIndeterminate = (Button) findViewById(R.id.label_indeterminate);
+	labelIndeterminate.setOnClickListener(this);
+
+	Button detailIndeterminate = (Button) findViewById(R.id.detail_indeterminate);
+	detailIndeterminate.setOnClickListener(this);
+
+	Button annularDeterminate = (Button) findViewById(R.id.annular_determinate);
+	annularDeterminate.setOnClickListener(this);
+
+	Button dimBackground = (Button) findViewById(R.id.dim_background);
+	dimBackground.setOnClickListener(this);
+
+    }
+
+    private BaseProgressDialog hud;
+
+    @Override
+    public void onClick(View v)
+    {
+	switch (v.getId())
+	{
+	    case R.id.indeterminate:
+		hud = new ProgressDialog(this);
+		scheduleDismiss();
+		break;
+	    case R.id.label_indeterminate:
+		hud = new ProgressDialog(this).setLabel("Please wait");
+		scheduleDismiss();
+		break;
+	    case R.id.detail_indeterminate:
+		hud = new ProgressDialog(this).setLabel("Please wait").setDetailsLabel(
+			"Downloading data");
+		scheduleDismiss();
+		break;
+
+	    case R.id.annular_determinate:
+		hud = new ProgressLoadDialog(this).setLabel("Please wait");
+		simulateProgressUpdate();
+		break;
+
+	    case R.id.dim_background:
+		hud = new ProgressDialog(this).setDimAmount(0.5f);
+		scheduleDismiss();
+		break;
+
+	}
+
+	hud.show();
+    }
+
+    private void simulateProgressUpdate()
+    {
+	if (hud instanceof ProgressLoadDialog)
+	{
+	    final ProgressLoadDialog progressLoadDialog = (ProgressLoadDialog) hud;
+	    progressLoadDialog.setMaxProgress(100);
+	    final Handler handler = new Handler();
+	    handler.postDelayed(new Runnable()
+	    {
+		int currentProgress;
+
+		@Override
+		public void run()
+		{
+		    currentProgress += 1;
+		    progressLoadDialog.setProgress(currentProgress);
+		    if (currentProgress < 100)
+		    {
+			handler.postDelayed(this, 50);
+		    }
+		}
+	    }, 100);
+	}
+    }
+
+    private void scheduleDismiss()
+    {
+	Handler handler = new Handler();
+	handler.postDelayed(new Runnable()
+	{
+	    @Override
+	    public void run()
+	    {
+		if (hud.isShowing())
+		{
+		    hud.dismiss();
+		}
+	    }
+	}, 2000);
+    }
+}
---
+++ JhProressbar/src/com/example/jhproressbar/view/ProgressDialog.java
@@ -0,0 +1,43 @@
+package com.example.jhproressbar.view;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 键盘 on 2016/1/26.
+ * 
+ */
+public class ProgressDialog extends BaseProgressDialog
+{
+    private static final float ANIMATE_SPEED = 1;
+    private SpinView mIndeterminateView;
+
+    @Override
+    protected View setContentView()
+    {
+	mIndeterminateView = new SpinView(getContext());
+	mIndeterminateView.setAnimationSpeed(ANIMATE_SPEED);
+	int widthMeasureSpec = Helper.dpToPixel(40, getContext());
+	int heightMeasureSpec = Helper.dpToPixel(40, getContext());// 改变加载图标大小
+	LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(widthMeasureSpec,
+		heightMeasureSpec);
+	mIndeterminateView.setLayoutParams(lp);
+	return mIndeterminateView;
+    }
+
+    public ProgressDialog(Context context)
+    {
+	super(context);
+    }
+
+    /**
+     * 改变动画的速度
+     * 
+     * @param scale 默认 1, 2 速度提升2倍, 0.5 速度减少半倍..等
+     */
+    public void setAnimationSpeed(int scale)
+    {
+	mIndeterminateView.setAnimationSpeed(scale);
+    }
+}
